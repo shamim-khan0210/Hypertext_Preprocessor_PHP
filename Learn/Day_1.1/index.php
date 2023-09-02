4ac1bde60fe6e387461a18150c0967b0c23b9bc2
shamim-khan0210 <132485646+shamim-khan0210@users.noreply.github.com>
2023-09-02 23:35:25 +0600

    variable_commentLine_print_DataType

---
+++ Learn/Day_1.1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $name = 'Shamim';
+        $name = 'Its me';
+        $age = 30;
+        $age1 = 30.3;
+        $nationality = true;
+        $people = array('Shamim', 'Khan', 'Hasibul');
+
+        var_dump($people);//to Show what type of this variable
+
+
+
+     ?>
+</body>
+</html>
