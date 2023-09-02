36553adadb212390e34a50e04d895f49d6d03f09
shamim-khan0210 <132485646+shamim-khan0210@users.noreply.github.com>
2023-09-02 19:44:45 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+</head>
+<body>
+    <?php
+        echo 'Shamim Khan';
+        echo 'Hasibul hasan';
+
+     ?>
+</body>
+</html>
