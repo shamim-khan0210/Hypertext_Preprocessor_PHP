40acac885294e13c9951cd6645fe67213db45222
shamim-khan0210 <132485646+shamim-khan0210@users.noreply.github.com>
2023-09-03 09:39:38 +0600

    Math_Function

---
+++ Learn/Day_2.1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Math Function
+        echo (pi()); //Show PI value
+        echo '<br>';
+
+        echo (max(1, 23, 24, 45)); // Show maximum value
+        echo '<br>';
+        echo (min(1, 23, 24, 45)); // Show minimum value
+        echo '<br>';
+
+        echo(abs(-10)); // Show Absulate number
+        echo '<br>';
+
+        echo(sqrt(16)); // Show Square root value
+        echo '<br>';
+
+        echo(rand(1, 6)); //show random number
+        echo '<br>';
+
+        echo (round(pi())); // Show round number from PI
+
+     ?>
+</body>
+</html>
